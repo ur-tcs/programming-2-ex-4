4e31c598a64847489cbae119b33ddff9f7e74bfe
franziska-a <147037823+franziska-a@users.noreply.github.com>
2024-05-08 11:10:17 +0200

    Delete calculator directory. Those were the unaltered epfl files, which we don't need

--- calculator/src/main/scala/calculator/Evaluator.scala
+++
@@ -1,4 +0,0 @@
-package calculator
-
-trait Evaluator[E <: Expr, R]:
-  def evaluate(e: E): R
--- calculator/src/main/scala/calculator/TinyEvaluator.scala
+++
@@ -1,8 +0,0 @@
-package calculator
-
-class TinyEvaluator extends Evaluator[TinyExpr, Double]:
-  import TinyExpr.*
-
-  /** Evaluate an expression to its value. */
-  def evaluate(e: TinyExpr): Double =
-    42
